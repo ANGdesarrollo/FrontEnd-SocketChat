1db18b53102300a8edc5d5447d4f0f743290a5db
Alexis Graff <alexisgraff123@gmail.com>
2023-01-29 19:09:12 -0300

    arreglo sound en chat

--- src/chat/components/menuChat/MenuChat.jsx
+++ src/chat/components/menuChat/MenuChat.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { onLogout } from "../../../store/slices/auth/index.js";
+import { handleSong } from "../../../store/slices/song/index.js";
 
 const imgMenu = 'https://res.cloudinary.com/dwz16rstr/image/upload/v1674089350/chat/Vector_2_feqlix.png';
 const linkedin = 'https://www.linkedin.com/in/alexis-graff-front-end';
@@ -14,7 +15,7 @@ export const MenuChat = () => {
     return (
         <div className="menu">
             <nav>
-                <li><a href="#">SOUND: { templateSound }</a></li>
+                <li><a onClick={() => dispatch(handleSong())} href="#">SOUND: { templateSound }</a></li>
                 <li><a href="#">THEME: DARK</a></li>
                 <li><a target="_blank" href={ linkedin }>CONTACT ME</a></li>
                 <li id="logout" onClick={ () => dispatch( onLogout() ) }><a>LOGOUT</a></li>
