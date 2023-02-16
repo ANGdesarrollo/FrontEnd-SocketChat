30b151e9fa4e10ed1a8f2a6cd7301ca89bc70265
Alexis Graff <alexisgraff123@gmail.com>
2023-02-16 20:04:34 -0300

    cambio a render

--- src/config/configAPI.js
+++ src/config/configAPI.js
@@ -1,3 +1,5 @@
-export const API_URL = 'https://backend-socketchat-production.up.railway.app'
+// export const API_URL = 'https://backend-socketchat-production.up.railway.app'
 // export const API_URL = 'http://localhost:8080'
+export const API_URL = 'https://space-chat-hhqs.onrender.com'
+
 
